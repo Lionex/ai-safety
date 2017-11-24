dc2f87363052bf781c0b716ba9af30bef5b1bb93
Gwen Lofman <Gwen@Lofman.co>
2017-11-24 13:03:47 -0500

    Display randomly initialized vectors in sketch
    Randomly initialize 100 vectors and have code to visualize the
    vectors, with dynamic size according to the shape of the window.
    Establishes a solid basis for running the feed-forward network.

--- sketch/nudge/sketch.js
+++ sketch/nudge/sketch.js
@@ -1,18 +1,54 @@
 function nudge( p ) {
 
-var x = 100;
-var y = 100;
+// Draw a 2D vector
+function draw_vec2(v, c) {
+    p.line(c[0], c[1], c[0] + v[0], c[1] + v[1])
+}
+
+// Container for the set of vectors which represent words in this visualization
+let vectors = [];
 
 p.setup = () => {
     p.createCanvas(window.innerWidth, window.innerHeight)
+
+    for (var i = 0; i < 100; i++) {
+        // Create normalized vectors pulled from a random uniform distribution,
+        // counts as random initialization of the network
+        let x = Math.random() * 2 - 1
+        let y = Math.random() * 2 - 1
+
+        let magnitude = Math.sqrt(x*x+y*y)
+
+        vectors.push([x, y].map((i) => i/magnitude))
+    }
 }
 
 p.draw = () => {
-    p.ellipse(p.mouseX, p.mouseY, 20, 20)
+    // Center position/maximum ellipse radius
+    let c = [window.innerWidth/2, window.innerHeight/2]
+
+    // size factor based on the window shape
+    let size_factor = Math.min(c[0], c[1]) * (3 / 4)
+
+    let max_size = vectors
+        .map((a) => Math.sqrt(a[0]*a[0]+a[1]*a[1]))
+        .reduce((a,b) => Math.max(a,b))
+
+    p.translate(c[0],c[1])
+    // Draw vectors from the center
+    vectors.map((v) => {
+        // resize the normalized vectors to the desired size before drawing them
+        // from the given position c
+        draw_vec2(v.map((i) => i*size_factor/max_size), [0,0])
+    })
 }
 
 p.mousePressed = () => {
-    p.clear()
+
+}
+
+p.mouseReleased = () => {
+
 }
 
 }
