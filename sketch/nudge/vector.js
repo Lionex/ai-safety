f798d314739c1f23f4fe5d3dfdd6a25f123beb01
Gwen Lofman <Gwen@Lofman.co>
2017-11-25 13:19:27 -0500

    Add missing WEVI assert

--- sketch/nudge/vector.js
+++ sketch/nudge/vector.js
@@ -180,3 +180,15 @@ function dot_product(a, b) {
     }
     return tmpSum;
 }
+
+// http://stackoverflow.com/questions/15313418/javascript-assert
+function assert(condition, message) {
+    if (!condition) {
+        message = message || "Assertion failed";
+        if (typeof Error !== "undefined") {
+            throw new Error(message);
+        }
+        throw message; // Fallback
+    }
+}
+
